42f202670ce070b3b42490cb348c2b34d032d43a
nataliagomes <naty.pinheiro95@gmail.com>
2019-07-25 16:56:51 -0300

    formulario,admin

--- website/models.py
+++ website/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+    GENERO= ( 
+        ('M' , 'Masculino'),
+        ('F','Feminino'),
+        ('O','Outros'),
+
+    )
+
+
+    nome = models.CharField(
+        max_length=255,
+        verbose_name= 'Nome'
+    )
+    sobrenome = models.CharField (
+        max_length=255,
+        verbose_name='sobrenome'
+    )       
+    genero = models.CharField(
+        max_length=255,
+        verbose_name='Genero',
+        choices=GENERO
+        #para o que conseguir prever 
+    )
+
+    email = models.EmailField(
+        max_length=255,
+        verbose_name='Email'
+
+    )
+    data_de_criacao = models.DateTimeField(auto_now_add=True)
+    ativo = models.BooleanField(default=True) 
+ 
+    def __str__(self):
+        return self.nome + ' ' + self.sobrenome 
+    
--- website/views.py
+++ website/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
+from website.models import Pessoa
 
 # Create your views here.
 
 def index(request):
-    contexto = {
+    contexto = {}
+    if request.method == 'post' :
+        Pessoa = Pessoa()
+        Pessoa.nome = request.post['nome']
 
-    }
+
+
+
     return render(request, 'index.html', contexto)
 
 
 def sobre(request):
+    Pessoa =Pessoa
     contexto = {
+
         
     }
     return render(request, 'sobre.html', contexto)
+#python manage.py makemigrations = gera arquivo de migração 
+#python manage.py  migrate = através do arquivo gerado ele cria ou modifica a base de dados 
